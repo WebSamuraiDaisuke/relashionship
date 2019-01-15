887795c453e20246ed7cc76f92676b3f76b5c9f2
Daisuke Minagawa <iamworkingwell@gmail.com>
2019-01-15 18:08:37 +0900

    implimenting login now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,38 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @session = session_params
+    # ここでsession_paramsを使う場合はどういうコードが必要か？
+    email = params[:session][:email]
+    password = params[:session][:password]
+    if login(email, password)
+      flash[:success] = 'ログインに成功しました'
+      redirect_to root_path
+    else
+      flash[:danger] = 'ログインに失敗しました'
+      render 'sessions/new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
   end
 
   private
 
   def session_params
-    params.require(:session).permit(:name, :password)
+    params.require(:session).permit(:email, :password)
+  end
+
+  def login(email, password)
+    @user = User.find_by(email: email)
+    #if @user && @user.authenticate(password)
+    if @user&.authenticate(password) # ここぼっち演算子を使えるのはなぜか？ぼっち演算子とは？
+      session[:user_id] = @user.id
+      true
+    else
+      false
+    end
   end
 end
